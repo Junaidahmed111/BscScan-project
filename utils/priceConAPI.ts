22b8274085ab08eff4a2f5d72998543c78869475
Junaidahmed111 <ahmedjuniad30@gmail.com>
2024-02-02 23:30:18 +0500

    fixing price con

--- utils/priceConAPI.ts
+++ utils/priceConAPI.ts
@@ -1,15 +1,15 @@
-const MY_API_KEY = "CG-DU5FCTAfpDMaPSdQKmzaaPsD";
-const API_URL =
-  "https://api.coingecko.com/api/v3/simple/price?ids=ethereum%2Cbitcoin%2Cbinancecoin%2Csolana&vs_currencies=pkr&include_24hr_change=true&include_last_updated_at=true&apikey=" +
-  MY_API_KEY;
+// const MY_API_KEY = "CG-DU5FCTAfpDMaPSdQKmzaaPsD";
+// const API_URL =
+//   "https://api.coingecko.com/api/v3/simple/price?ids=ethereum%2Cbitcoin%2Cbinancecoin%2Csolana&vs_currencies=pkr&include_24hr_change=true&include_last_updated_at=true&apikey=" +
+//   MY_API_KEY;
 
-export const fetchConversionApi = async () => {
-  try {
-    const response = await fetch(API_URL);
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.log("Error", error);
-    return null;
-  }
-};
+// export const fetchConversionApi = async () => {
+//   try {
+//     const response = await fetch(API_URL);
+//     const data = await response.json();
+//     return data;
+//   } catch (error) {
+//     console.log("Error", error);
+//     return null;
+//   }
+// };
